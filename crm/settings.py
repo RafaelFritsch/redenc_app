76f2d076cdd43da10e72dd6b02e26d9e78f0c333
RafaelFritsch <dorafa@gmail.com>
2023-12-13 18:35:50 -0300

    ajustes docker

--- crm/settings.py
+++ crm/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l0&74_o6pw3*#4$j9@^$n56a!q3^-c_o79vbur2mb0(w4vs28t'
-
+#SECRET_KEY = 'django-insecure-l0&74_o6pw3*#4$j9@^$n56a!q3^-c_o79vbur2mb0(w4vs28t'
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+#DEBUG = True
+DEBUG = os.getenv('DEBUG')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -85,11 +85,11 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME':'redenc',
-        'USER':'postgres',
-        'PASSWORD':'AGkgFq44X4lfQ9z7cgl1O',
-        'HOST':'localhost',
-        'PORT':'5432',
+        'NAME': os.environ.get('DB_NAME', 'redenc'),
+        'USER': os.environ.get('DB_USER', 'portgres'),
+        'PASSWORD': os.environ.get('DB_PASS', 'AGkgFq44X4lfQ9z7cgl1O'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+#STATIC_ROOT ='static/'
 STATIC_ROOT = [
     os.path.join(BASE_DIR, 'static'),
 ]
